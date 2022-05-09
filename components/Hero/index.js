96983b9d873765ebb6d1711a9378aad650039ef4
BrijeshMakwana18 <bamakwana2000@gmail.com>
2022-05-09 22:00:03 +0530

    hero component created

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,147 +1,21 @@
 import { useEffect, useRef, useState } from "react";
-import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMoon, faSun } from "@fortawesome/free-regular-svg-icons";
-import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-import {
-  Container,
-  ThemeToggle,
-  ProfileBorder,
-  ProfileContainer,
-  Name,
-  Profession,
-  SocialContainer,
-  ProfileSocialLink,
-  InfoContainer,
-  InfoItem,
-  InfoNumber,
-  InfoLabel,
-  ButtonContainer,
-  ButtonTitle,
-  FilterToggle,
-  FilterButton,
-  ProjectContainer,
-  ProjectCard,
-  SkillContainer,
-  SkillCard,
-  EducationContainer,
-  EducationCard,
-} from "./styles";
+import { Container, Heading, BigHeading, Desc, Resume } from "./styles";
 
-export default function Hero({ theme, toggleTheme, projects, skills }) {
-  const profile = useRef(null);
-  const name = useRef(null);
-  const profession = useRef(null);
-  const social = useRef(null);
-  const infoGroup = useRef(null);
-  const button = useRef(null);
-  const content = useRef(null);
-  const filters = useRef(null);
-  const sections = useRef(null);
-
-  const [filter, setFilter] = useState("projects");
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      async function animate() {
-        const ScrollReveal = require("../../public/scrollreveal").default;
-        const sr = ScrollReveal({
-          origin: "top",
-          distance: "60px",
-          duration: 2500,
-          delay: 400,
-        });
-        sr.reveal(profile?.current);
-        sr.reveal(name?.current, { delay: 100 });
-        sr.reveal(profession?.current, { delay: 200 });
-        sr.reveal(social?.current, { delay: 300 });
-        sr.reveal(infoGroup?.current, { interval: 100, delay: 300 });
-        sr.reveal(button?.current, { delay: 400 });
-        sr.reveal(content, { delay: 400 });
-        sr.reveal(filters?.current, { delay: 600 });
-        sr.reveal(sections?.current, { delay: 800 });
-      }
-      animate();
-    }
-  }, []);
-  const handleFilterChange = (filter) => {
-    setFilter(filter);
-  };
+export default function Hero() {
   return (
     <Container>
-      <ThemeToggle onClick={toggleTheme}>
-        <FontAwesomeIcon
-          className="toggle-image"
-          icon={theme === "light" ? faMoon : faSun}
-        />
-      </ThemeToggle>
-      <ProfileBorder ref={profile}>
-        <ProfileContainer>
-          <Image src={"/profile.png"} height="100%" width={"100%"} />
-        </ProfileContainer>
-      </ProfileBorder>
-      <Name ref={name}>Brijesh Makwana</Name>
-      <Profession ref={profession}>Software Engineer</Profession>
-      <SocialContainer ref={social}>
-        <ProfileSocialLink
-          target={"_blank"}
-          href="https://in.linkedin.com/in/brijesh-makwana-a876b116b"
-          mode={theme}
-        >
-          <FontAwesomeIcon className="link" icon={faLinkedin} />
-        </ProfileSocialLink>
-        <ProfileSocialLink
-          target={"_blank"}
-          href="https://github.com/BrijeshMakwana18"
-          mode={theme}
-        >
-          <FontAwesomeIcon className="link" icon={faGithub} />
-        </ProfileSocialLink>
-      </SocialContainer>
-      <InfoContainer ref={infoGroup}>
-        <InfoItem>
-          <InfoNumber>4</InfoNumber>
-          <InfoLabel>Months of experience</InfoLabel>
-        </InfoItem>
-        <InfoItem>
-          <InfoNumber>3</InfoNumber>
-          <InfoLabel>Projects completed</InfoLabel>
-        </InfoItem>
-      </InfoContainer>
-      <ButtonContainer ref={button} download="" href="/Brijesh_Resume.pdf">
-        <ButtonTitle>Download CV</ButtonTitle>
-      </ButtonContainer>
-      <FilterToggle ref={filters}>
-        <FilterButton
-          onClick={() => handleFilterChange("projects")}
-          active={filter === "projects"}
-        >
-          Projects
-        </FilterButton>
-        <FilterButton
-          onClick={() => handleFilterChange("skills")}
-          active={filter === "skills"}
-        >
-          Skills
-        </FilterButton>
-      </FilterToggle>
-      {filter === "projects" && (
-        <ProjectContainer ref={sections}>
-          {projects.map((item, index) => {
-            return (
-              <ProjectCard key={index}>
-                <img src={item.image} />
-              </ProjectCard>
-            );
-          })}
-        </ProjectContainer>
-      )}
-      {filter === "skills" && (
-        <SkillContainer ref={sections}>
-          {skills.map((item, index) => {
-            return <SkillCard key={index}>{item}</SkillCard>;
-          })}
-        </SkillContainer>
-      )}
+      <Heading>Hi, my name is</Heading>
+      <BigHeading>Brijesh Makwana</BigHeading>
+      <BigHeading opacity={0.7} fontSize={"3vw"}>
+        I build exceptional digital experiences.
+      </BigHeading>
+      <Desc>
+        {
+          "I’m a software engineer specializing in building mobile apps and learning new tech every day. Currently, I’m focused on building e-commerce products at "
+        }
+        <span>Alshaya Group</span>.
+      </Desc>
+      <Resume>Resume</Resume>
     </Container>
   );
 }
--- components/Hero/styles.js
+++ components/Hero/styles.js
@@ -1,272 +1,54 @@
 import styled from "styled-components";
-
+import { fonts } from "../../utils/theme";
 export const Container = styled.div`
   display: flex;
-  margin-left: auto;
-  margin-right: auto;
-  padding-top: 3.5rem;
-  align-items: center;
+  justify-content: center;
   flex-direction: column;
+  width: 100%;
+  padding: 30vh 0vh 30vh 0vh;
+  /* background-color: green; */
 `;
 
-export const ThemeToggle = styled.div`
-  position: absolute;
-  height: 20px;
-  width: 20px;
-  right: 24vw;
-  top: 3.5vh;
-  align-items: center;
-  justify-content: center;
-  .toggle-image {
-    height: 25px;
-    width: 25px;
+export const Heading = styled.text`
+  font-size: 1.5vw;
+  font-family: ${fonts.PoppinsMedium};
+  color: ${(props) => props.theme.primaryBlue};
+`;
+
+export const BigHeading = styled.text`
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "3vw")};
+  font-family: ${fonts.PoppinsBold};
+  color: ${(props) => props.theme.primaryFontColor};
+  opacity: ${(props) => (props.opacity ? props.opacity : 1)};
+  margin-top: 1vh;
+`;
+
+export const Desc = styled.p`
+  font-size: 1vw;
+  font-family: ${fonts.Poppins};
+  opacity: 0.8;
+  width: 50%;
+  margin-top: 1vh;
+  span {
+    color: ${(props) => props.theme.primaryPink};
+    font-family: ${fonts.PoppinsMedium};
     cursor: pointer;
-    -webkit-touch-callout: none; /* iOS Safari */
-    -webkit-user-select: none; /* Safari */
-    -khtml-user-select: none; /* Konqueror HTML */
-    -moz-user-select: none; /* Old versions of Firefox */
-    -ms-user-select: none; /* Internet Explorer/Edge */
-    user-select: none;
   }
 `;
 
-export const ProfileBorder = styled.div`
-  border: 3.5px solid ${(props) => props.theme.blue};
-  justify-self: center;
-  width: 115px;
-  height: 115px;
-  border-radius: 50%;
-  display: grid;
-  place-items: center;
-  margin-bottom: 0.75rem;
-  @media screen and (min-width: 992px) {
-    width: 135px;
-    height: 135px;
-    margin-bottom: 1rem;
-  }
-`;
-
-export const ProfileContainer = styled.div`
-  height: 95%;
-  width: 95%;
-  background: linear-gradient(
-    180deg,
-    hsla(207, 90%, 61%, 1) 0%,
-    hsla(207, 90%, 61%, 0.2) 100%
-  );
-  border-radius: 50%;
-  overflow: hidden;
-  display: flex;
-  align-items: flex-end;
-  justify-content: center;
-`;
-
-export const Name = styled.h2`
-  /* font-family: "PoppinsBold", sans-serif; */
-`;
-
-export const Profession = styled.h3`
-  font-size: 1rem;
-  font-weight: 500;
-  color: ${({ theme }) => theme.light};
-  margin-bottom: 1rem;
-`;
-
-export const SocialContainer = styled.ul`
-  display: flex;
-  justify-content: center;
-  column-gap: 1rem;
-`;
-
-export const ProfileSocialLink = styled.a`
-  font-size: 1.5rem;
-  color: ${({ theme }) => theme.link};
+export const Resume = styled.button`
   cursor: pointer;
-  -webkit-touch-callout: none; /* iOS Safari */
-  -webkit-user-select: none; /* Safari */
-  -khtml-user-select: none; /* Konqueror HTML */
-  -moz-user-select: none; /* Old versions of Firefox */
-  -ms-user-select: none; /* Internet Explorer/Edge */
-  user-select: none;
-  .link {
-    transition: 0.5s;
-    color: ${(props) => (props.mode === "light" ? "#0E151C" : "#FAFDFF")};
-  }
-  &:hover {
-    .link {
-      color: ${({ theme }) => theme.linkHover};
-    }
-  }
-`;
-
-export const InfoContainer = styled.ul`
-  display: flex;
-  justify-content: center;
-  column-gap: 10%;
-`;
-
-export const InfoItem = styled.div`
-  margin-top: 3vh;
-  display: flex;
-  width: 30%;
-  flex-direction: column;
-  align-items: center;
-  text-align: center;
-`;
-
-export const InfoNumber = styled.h2`
-  font-size: 1rem;
-  margin-top: 0.5vh;
-`;
-
-export const InfoLabel = styled.p`
-  font-size: 0.85rem;
-  font-weight: 500;
-  margin-top: 0.5vh;
-  color: ${({ theme }) => theme.light};
-`;
-
-export const ButtonContainer = styled.a`
-  text-decoration: none;
-  display: flex;
-  align-items: center;
-  column-gap: 0.5rem;
-  background-color: hsl(207, 90%, 61%);
-  color: #fff;
-  padding: 1.15rem 1.5rem;
-  border-radius: 0.5rem;
-  box-shadow: 0 8px 24px hsla(207, 90%, 61%, 0.25);
+  height: 5vh;
+  width: 10vw;
+  border-radius: 0.3vw;
+  border-width: 1px;
+  background-color: transparent;
+  border-color: ${(props) => props.theme.primaryBlue};
+  color: ${(props) => props.theme.primaryFontColor};
+  font-size: 1vw;
+  font-family: ${fonts.PoppinsMedium};
   margin-top: 2vh;
-  cursor: pointer;
-`;
-
-export const ButtonTitle = styled.h2`
-  font-size: 1.25rem;
-`;
-
-export const FilterToggle = styled.div`
-  margin: 2rem 0 2.5rem;
-  background-color: ${({ theme }) => theme.filterToggle};
-  padding: 0.375rem;
-  border-radius: 0.75rem;
-  display: flex;
-  justify-content: space-between;
-  column-gap: 0.5rem;
-  align-items: center;
-  text-align: center;
-`;
-
-export const FilterButton = styled.div`
-  border: none;
-  outline: none;
-  padding: 1rem 3rem 1rem 3rem;
-  font-size: 0.85rem;
-  font-weight: 500;
-  border-radius: 0.75rem;
-  cursor: pointer;
-  transition: 0.5s;
-  background-color: ${(props) =>
-    props.active ? props.theme.filterButton : "transparent"};
   &:hover {
-    background-color: ${({ theme }) => theme.filterButton};
-  }
-`;
-
-export const ProjectContainer = styled.div`
-  padding: 0px 30px 0px 26px;
-  display: grid;
-  grid-gap: 25px;
-  gap: 25px;
-  width: 90vw;
-  @media screen and (min-width: 678px) {
-    grid-template-columns: repeat(1, minmax(0, 1fr));
-  }
-  @media screen and (min-width: 679px) and (max-width: 1200px) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-  }
-  @media screen and (min-width: 1201px) {
-    grid-template-columns: repeat(3, minmax(0, 1fr));
-  }
-`;
-
-export const ProjectCard = styled.div`
-  height: 30vh;
-  overflow: hidden;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-  position: relative;
-  img {
-    height: 100%;
-    width: 100%;
-    object-fit: cover;
-    border-radius: 10px;
-  }
-`;
-
-export const SkillContainer = styled.div`
-  display: grid;
-  grid-gap: 25px;
-  gap: 25px;
-  width: 60vw;
-  @media screen and (min-width: 678px) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-  }
-  @media screen and (min-width: 679px) and (max-width: 1200px) {
-    grid-template-columns: repeat(3, minmax(0, 1fr));
-  }
-  @media screen and (min-width: 1201px) {
-    grid-template-columns: repeat(4, minmax(0, 1fr));
-  }
-`;
-
-export const SkillCard = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  column-gap: 0.5rem;
-  background-color: ${({ theme }) => theme.filterToggle};
-  color: ${({ theme }) => theme.light};
-  padding: 1.15rem 1.5rem;
-  border-radius: 0.5rem;
-  margin-top: 2vh;
-  cursor: pointer;
-  text-align: center;
-  &:hover {
-    background-color: ${({ theme }) => theme.blue};
-    box-shadow: 0 8px 24px hsla(207, 90%, 61%, 0.25);
-    color: #fff;
-  }
-`;
-
-export const EducationContainer = styled.div`
-  padding: 0px 30px 0px 26px;
-  display: grid;
-  grid-gap: 25px;
-  gap: 25px;
-  width: 90vw;
-  @media screen and (min-width: 678px) {
-    grid-template-columns: repeat(1, minmax(0, 1fr));
-  }
-  @media screen and (min-width: 679px) and (max-width: 1200px) {
-    grid-template-columns: repeat(2, minmax(0, 1fr));
-  }
-  @media screen and (min-width: 1201px) {
-    grid-template-columns: repeat(3, minmax(0, 1fr));
-  }
-`;
-
-export const EducationCard = styled.div`
-  height: 24vh;
-  border-radius: 20px;
-  overflow: hidden;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-  position: relative;
-  img {
-    height: 100%;
-    width: 100%;
-    object-fit: contain;
+    background-color: ${(props) => props.theme.primaryBlue};
   }
 `;
